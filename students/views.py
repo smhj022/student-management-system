7e066237b6c9fb860d8be1323d34909c1076a2f2
smhj022 <smhj022@gmail.com>
2021-07-27 14:09:41 +0000

    adding templates base.html and index.html

--- students/views.py
+++ students/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1>Welcome to Student managemnet system</h1>")
+    return render(request, "students/index.html")
 
 
