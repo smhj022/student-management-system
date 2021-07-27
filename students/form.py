ed752ec0952c5ec7df82d10d12047f5e63d9e65d
smhj022 <smhj022@gmail.com>
2021-07-27 18:48:41 +0000

    form Update and add post method to form

--- students/form.py
+++ students/form.py
@@ -1,12 +1,28 @@
 from django import forms
-from django.core.validators import MinValueValidator, MaxValueValidator
 
-class_choice = ["LKG", "UKG", "1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th", "10th"]
-gender = ["Male", "Female", "Other"]
+class_choice = [
+    ("LKG", "LKG"),
+    ("UKG", "UKG"),
+    ("1st", "1st"),
+    ("2nd", "2nd"),
+    ("3rd", "3rd"),
+    ("4th", "4th"),
+    ("5th", "5th"),
+    ("6th", "6th"),
+    ("7th", "7th"),
+    ("8th", "8th"),
+    ("9th", "9th"),
+    ("10th",  "10th")
+]
+gender = [
+    ("Male", "Male"),
+    ("Female", "Female"),
+    ("Other", "Other")
+]
 
 
 class StudentRegistration(forms.Form):
     name = forms.CharField(max_length=30)
-    standard = forms.IntegerField(validators=[MaxValueValidator(12), MinValueValidator(1)])
-    gender = forms.CharField(max_length=5)
+    standard = forms.CharField(label="Standard", widget=forms.Select(choices=class_choice))
+    gender = forms.CharField(label="Gender", widget=forms.Select(choices=gender))
     city = forms.CharField(max_length=30)
--- students/views.py
+++ students/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 # from django.http import HttpResponse
 from .form import StudentRegistration
 from django.views import View
@@ -13,6 +14,18 @@ def index(request):
 class StudentEntry(View):
     def get(self, request):
         form = StudentRegistration()
+        return render(request, "students/registration.html", {
+            "form": form
+        })
+
+    def post(self, request):
+        form = StudentRegistration(request.POST)
+
+        if form.is_valid:
+            data = request.POST
+            print(data)
+            return HttpResponseRedirect("/")
+
         return render(request, "students/registration.html", {
             "form": form
         })
